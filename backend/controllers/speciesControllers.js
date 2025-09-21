464e0ca091552ad93ef9e4450b2dfee354dd34ad
Perwira-AZ <perwiraakhdanz@gmail.com>
2025-09-22 06:09:41 +0700

    feat: add fetch image (back-end)

--- backend/controllers/speciesControllers.js
+++ backend/controllers/speciesControllers.js
@@ -15,6 +15,21 @@ const pool = mysql.createPool({
 const getAllSpecies = async (req, res) => {
   try {
     const [rows] = await pool.query('SELECT * FROM species');
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: 'Species not found' });
+    }
+
+    for (const row of rows) {
+      const [imageRows] = await pool.query(
+        'SELECT image.link_id FROM image WHERE is_spec IS TRUE AND spec_arti_id = ?',
+        [row.id]
+      );
+      imageRows && imageRows[0]
+        ? (row.image = imageRows[0].link_id)
+        : (row.image = null);
+    }
+
     res.status(200).json(rows);
   } catch (err) {
     res.status(500).json(err);
@@ -26,7 +41,21 @@ const getSpecies = async (req, res) => {
     const [rows] = await pool.query('SELECT * FROM species WHERE id = ?', [
       req.params.id,
     ]);
-    res.status(200).json(rows);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: 'Species not found' });
+    }
+
+    // Get all images for that species
+    const [imageRows] = await pool.query(
+      'SELECT image.link_id FROM image WHERE is_spec IS TRUE AND spec_arti_id = ?',
+      [req.params.id]
+    );
+
+    // Attach images as an array
+    rows[0].images = imageRows.map((img) => img.link_id);
+
+    res.status(200).json(rows[0]);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -66,6 +95,21 @@ const searchSpecies = async (req, res) => {
     const values = [`%${keyword}%`];
 
     const [rows] = await pool.query(query, values);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: 'Species not found' });
+    }
+
+    for (const row of rows) {
+      const [imageRows] = await pool.query(
+        'SELECT image.link_id FROM image WHERE is_spec IS TRUE AND spec_arti_id = ?',
+        [row.id]
+      );
+      imageRows && imageRows[0]
+        ? (row.image = imageRows[0].link_id)
+        : (row.image = null);
+    }
+
     res.status(200).json(rows);
   } catch (err) {
     res.status(500).json(err);
